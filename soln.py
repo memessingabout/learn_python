4f210e7b51861b0ce9ca99d557ce6d1d00496a71
Benard Karanja <152717293+n-cognto@users.noreply.github.com>
2025-06-06 06:56:24 +0300

    Refined day 4 and 5

---
+++ soln.py
@@ -0,0 +1,18 @@
+my_list = [1, 2, 3, 4, 5]
+
+# for i in range(len(my_list)):
+#     print(f"Element {i}: {my_list[i]}")
+
+
+# for i in range(10):   #1 - 9
+#     print(i ,end=" ")  # Print numbers from 0 to 9 with space separator
+
+
+
+# if text == text[::-1]:
+#     return "They are palindrome"
+# else:
+#     return "They aren't"
+
+text = "Strip Python"
+print(text.split('Python'))
