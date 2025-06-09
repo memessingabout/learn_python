3bde4eb5100ec1695afc9bcec5e92cff8bd814bb
Benard Karanja <152717293+n-cognto@users.noreply.github.com>
2025-06-09 17:40:31 +0300

    Day 7 of learning

--- soln.py
+++ soln.py
@@ -1,9 +1,22 @@
-# text = "Python is awesome!"
-#  strip  , split , count, find, replace, isalpha, isdigit, isalnum, islower, isupper, istitle
-# print(text.count("o"))
 
+# nums = [1,1,2,3,4,5,6,6,6,7,7,8,9,]
 
-text = "Python Is awesome And Cool" # Is   I = 7
-# print(text)
-# print(text.strip(" awesome!"))
-print(text.find("Is"))
+# Remove duplicates using set
+# unique_nums = set(nums)
+
+# two pointers technique
+# def remove_duplicates(nums):
+#     if not nums:
+#         return "The list is empty"
+#         break
+    
+    
+nums = []
+
+def check_return(nums):
+    if not nums:
+        print("The list is empty")
+
+check_return(nums)
+print(val_ue)
+
