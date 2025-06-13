058b32537356621cb4e43d9fa28c57186fd01a0c
Benard Karanja <152717293+n-cognto@users.noreply.github.com>
2025-06-13 18:12:40 +0300

    Production version

--- soln.py
+++ soln.py
@@ -1,22 +1,24 @@
-# create user
-# username, email, password
+# # create user
+# # username, email, password
 
-if username == "":
-    print("Username cannot be empty")
-    if email == "":
-        print("Email cannot be empty")
-        if password == "":
-            print("Password cannot be empty")
+# if username == "":
+#     print("Username cannot be empty")
+#     if email == "":
+#         print("Email cannot be empty")
+#         if password == "":
+#             print("Password cannot be empty")
 
-            # Logic to create user
+#             # Logic to create user
 
-# Guarded clauses - early return conditions
-if not username:
-    print("Username cannot be empty")
-if not email:
-    print("Email cannot be empty")
-if not password:    
-    print("Password cannot be empty")
-     # Exit early if any field is empty
+# # Guarded clauses - early return conditions
+# if not username:
+#     print("Username cannot be empty")
+# if not email:
+#     print("Email cannot be empty")
+# if not password:    
+#     print("Password cannot be empty")
+#      # Exit early if any field is empty
 
-# Logic to create user
+# # Logic to create user
+
+
