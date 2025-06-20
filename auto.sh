8947afc88fa6aee79ca385e2e66979d89dcc4d2d
Benard Karanja <152717293+n-cognto@users.noreply.github.com>
2025-06-20 14:44:36 +0300

    Deleted unused

--- auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..30}
-do
-  filename="day_${i}.py"
-  echo '""" Today we will learn ... """' > "$filename"
-  echo "$filename created."
-done
-echo "All files created successfully."
